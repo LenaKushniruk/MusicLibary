1d01f42b9609efd09cdff135b13fd6865881e67a
LenaKushniruk <114736070+LenaKushniruk@users.noreply.github.com>
2023-10-06 10:01:10 +0200

    Add files via upload

---
+++ data.php
@@ -0,0 +1,86 @@
+<?php
+
+    $libary =[
+    
+    [
+        'id' => 28,
+        'artiest' => 'Dua Lipa',
+        'title' => 'Dance The Night',
+        'foto' => '',
+        'gerne' => 'POP',
+    ],
+    [
+        'id' => 28,
+        'artiest' => 'Imagine Dragons',
+        'title' => 'Enemy',
+        'foto' => '',
+        'gerne' => 'POP/Electro ',
+
+    ],
+    [
+        'id' => 28,
+        'artiest' => '',
+        'title' => '',
+        'foto' => '',
+        'gerne' => 'POP',
+
+    ],
+    [
+        'id' => 28,
+        'artiest' => 'Dua Lipa',
+        'title' => '',
+        'foto' => '',
+        'gerne' => 'POP',
+
+    ],
+    [
+        'id' => 28,
+        'artiest' => 'Dua Lipa',
+        'title' => '',
+        'foto' => '',
+        'gerne' => 'POP',
+
+    ],
+    [
+        'id' => 28,
+        'artiest' => 'Dua Lipa',
+        'title' => '',
+        'foto' => '',
+        'gerne' => 'POP',
+
+    ],
+    [
+        'id' => 28,
+        'artiest' => 'Dua Lipa',
+        'title' => '',
+        'foto' => '',
+        'gerne' => 'POP',
+
+    ],
+    [
+        'id' => 28,
+        'artiest' => 'Dua Lipa',
+        'title' => '',
+        'foto' => '',
+        'gerne' => 'POP',
+
+    ],
+    [
+        'id' => 28,
+        'artiest' => 'Dua Lipa',
+        'title' => '',
+        'foto' => '',
+        'gerne' => 'POP',
+
+    ],
+    [
+        'id' => 28,
+        'artiest' => 'Dua Lipa',
+        'title' => '',
+        'foto' => '',
+        'gerne' => 'POP',
+
+    ],
+
+    ];
+?>
---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>muziek</title>
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+<header>
+    <h2>Menu</h2>
+</header>
+<div>
+    <img src="" alt="">
+<?php
+
+
+
+
+?>
+</div>
+</body>
+</html>
+
